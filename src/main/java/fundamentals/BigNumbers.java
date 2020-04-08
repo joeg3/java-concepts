224ab3f52e58f10f18bb1db2a1aed88a2a1b1287
joe <jgliniecki@gmail.com>
2020-04-08 13:03:41 -0500

    Add checkstyle and correct formatting

--- src/main/java/fundamentals/BigNumbers.java
+++ src/main/java/fundamentals/BigNumbers.java
@@ -4,16 +4,15 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class BigNumbers {
-	
-	public BigInteger useBigInteger() {
-		BigInteger i = BigInteger.valueOf(3000000000000000000L);
-		return i.multiply(BigInteger.valueOf(10)); // This would be out of range for a long
-	}
-	
-	public BigDecimal useBigDecimal() {
-		// Using a double to do floating point subtraction of 2.0 - 1.1 is 0.899999999.
-		// Using BigDecimal gives an exact 0.9
-		return BigDecimal.valueOf(2, 0).subtract(BigDecimal.valueOf(11, 1));
-	}
 
+  public BigInteger useBigInteger() {
+    BigInteger i = BigInteger.valueOf(3000000000000000000L);
+    return i.multiply(BigInteger.valueOf(10)); // This would be out of range for a long
+  }
+
+  public BigDecimal useBigDecimal() {
+    // Using a double to do floating point subtraction of 2.0 - 1.1 is 0.899999999.
+    // Using BigDecimal gives an exact 0.9
+    return BigDecimal.valueOf(2, 0).subtract(BigDecimal.valueOf(11, 1));
+  }
 }
--- src/main/java/strings/Strings.java
+++ src/main/java/strings/Strings.java
@@ -1,40 +1,38 @@
 package strings;
 
 public class Strings {
-	
-	/**
-	 * This uses the compareTo() method which compares based on the Unicode values
-	 * of characters, which is not always in order for human readers. For language 
-	 * specific sorting rules, use Collator.
-	 */
-	public int useCompareTo(String str1, String str2) {
-		return str1.compareTo(str2);
-	}
-	
-	/**
-	 * Often, string formatting is used with System.out.printf() to send formatted string
-	 * to std out.  Here is an example of using String.format() to save the formatted string.
-	 */
-	public String formatString(String str, String name, int age) {
-		return String.format(str, name, age);
-	}
 
-	public String join(String delimiter, String... strings) {
-		return String.join(delimiter, strings);
-	}
+  /**
+   * This uses the compareTo() method which compares based on the Unicode values
+   * of characters, which is not always in order for human readers. For language
+   * specific sorting rules, use Collator.
+   */
+  public int useCompareTo(String str1, String str2) {
+    return str1.compareTo(str2);
+  }
 
-	public String[] split(String delimiter, String str) {
-		return str.split(delimiter);
-	}
+  /**
+   * Often, string formatting is used with System.out.printf() to send formatted string
+   * to std out.  Here is an example of using String.format() to save the formatted string.
+   */
+  public String formatString(String str, String name, int age) {
+    return String.format(str, name, age);
+  }
 
-	public String stringBuilder(String... strings) {
-		StringBuilder builder = new StringBuilder();
-		for (String s : strings) {
-			builder.append(s);
-		}
+  public String join(String delimiter, String... strings) {
+    return String.join(delimiter, strings);
+  }
 
-		return builder.toString();
-	}
-	
+  public String[] split(String delimiter, String str) {
+    return str.split(delimiter);
+  }
 
+  public String stringBuilder(String... strings) {
+    StringBuilder builder = new StringBuilder();
+    for (String s : strings) {
+      builder.append(s);
+    }
+
+    return builder.toString();
+  }
 }
